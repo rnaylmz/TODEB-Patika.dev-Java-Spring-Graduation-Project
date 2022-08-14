81463be65403e93068cacda77ce38b2e95f687ba
Rana Yılmaz <rnaryhn@gmail.com>
2022-08-15 01:25:08 +0300

    test services added

--- src/test/java/com/todeb/rnaylmz/creditapplicationsystem/testservice/CustomerServiceTest.java
+++ src/test/java/com/todeb/rnaylmz/creditapplicationsystem/testservice/CustomerServiceTest.java
@@ -1,13 +1,27 @@
 package com.todeb.rnaylmz.creditapplicationsystem.testservice;
 
+import com.todeb.rnaylmz.creditapplicationsystem.repository.ICustomerRepository;
+import com.todeb.rnaylmz.creditapplicationsystem.service.CustomerService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
 @ExtendWith(MockitoExtension.class)
 public class CustomerServiceTest {
 
+    @Mock
+    private ICustomerRepository customerRepository;
 
+    @InjectMocks
+    private CustomerService customerService;
+
+    @Test
+    void getAllCustomers(){
+
+    }
 
 
 }
